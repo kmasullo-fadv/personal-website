85712d710ac30146d902ce743b8c34fe3c2f5b22
SoundSlab <administrator@Mac-mini.local>
2020-11-08 18:07:19 -0500

    music listen started

--- src/MusListen/MusListen.js
+++ src/MusListen/MusListen.js
@@ -10,11 +10,12 @@ export default class MusListen extends Component {
         return (
             <div className="musListen">
                 <Iframe 
-                    url="https://bandcamp.com/EmbeddedPlayer/album=133674501/size=large/bgcol=333333/linkcol=e99708/tracklist=true/transparent=true/"
+                    url="https://bandcamp.com/EmbeddedPlayer/album=133674501/size=large/bgcol=333333/linkcol=e99708/artwork=small/transparent=true/"
                     position="absolute"
-                    width="50%"
+                    width="100%"
                     className="player"
                     height="100%"
+                    seamless
                 />
             </div>
         )
