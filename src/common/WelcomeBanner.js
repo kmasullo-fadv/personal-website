6707ef0b66e311fc8cbd956730a7e8098eb30d1a
Kameron Masullo <k_masullo@outlook.com>
2022-01-16 17:29:28 -0500

    Banner text centered

--- src/common/WelcomeBanner.js
+++ src/common/WelcomeBanner.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const WelcomeBanner = (props) => {
     return (
-        <div className="welcomeBanner">
+        <div className="welcomeBanner d-flex align-items-center">
             <video className="portfolioVid" autoPlay muted loop>
                 <source src={props.src} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
-            <div className="headerText">
+            <div className="headerText row d-flex align-items-center p-5">
                 <h2 className="px65">Welcome</h2>
                 <h2 className="px50">{props.message}</h2>
             </div>
