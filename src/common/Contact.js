7e629db6baf164e5914e4adc67fb28450607e278
Kameron Masullo <k_masullo@outlook.com>
2022-01-21 16:59:18 -0500

    Contact form css change

--- src/common/Contact.js
+++ src/common/Contact.js
@@ -4,23 +4,25 @@ import { Modal } from "react-bootstrap";
 const Contact = (props) => {
     return (
         <Modal show={props.show} onHide={props.handleClose} centered>
-            <Modal.Header closeButton>
+            <Modal.Header closeButton style={{ background: "rgb(243, 248, 254)", border: "none" }}>
                 <Modal.Title>Get in touch!</Modal.Title>
             </Modal.Header>
             <Modal.Body>{
-                <form id="contactForm" action="https://formspree.io/xvowbnak" method="post" onSubmit={null}>
+                <form className="mt-3 mb-3" id="contactForm" action="https://formspree.io/xvowbnak" method="post" onSubmit={null}>
                     <label htmlFor="user-name">Name:</label><br/>
                     <input className="w-100" id="user-name" type="text" name="name" required /><br/>
 
-                    <label className="mt-3" htmlFor="user-email">Email:</label><br/>
+                    <label className="mt-4" htmlFor="user-email">Email:</label><br/>
                     <input className="w-100" id="user-email" type="text" name="email" required /><br/>
 
-                    <label className="mt-3" htmlFor="user-message">Message:</label><br/>
+                    <label className="mt-4" htmlFor="user-message">Message:</label><br/>
                     <textarea className="w-100" id="user-message" name="message" required /><br/>
-                    <button className="w-100 btn btn-primary mt-3" type="submit">Submit</button>
+                    <button className="w-100 btn btn-primary mt-4" type="submit">Submit</button>
                 </form>
             }</Modal.Body>
-            <Modal.Footer><p className="w-100 text-center">You may also email me at k@kameronmasullo.com</p></Modal.Footer>
+            <Modal.Footer style={{ background: "rgb(243, 248, 254)", border: "none" }}>
+                <p className="w-100 text-center">You may also email me at k@kameronmasullo.com</p>
+            </Modal.Footer>
         </Modal>
     )
 }
