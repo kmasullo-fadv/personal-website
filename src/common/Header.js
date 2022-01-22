edc15d4b5a1b564c4acdada42982f1b98e5bc790
Kameron Masullo <k_masullo@outlook.com>
2022-01-22 00:41:13 -0500

    social added to music header

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import MusicSocial from '../music/MusicSocial';
 
 const DEV = "dev";
 const MUSIC = "music"
@@ -9,12 +10,10 @@ const Header = (props) => {
             <header>
                 <Link to="/" className="headerLink"><h1 className="headerH1 hover">Kameron Masullo</h1></Link>
                 <nav>
+                    { props.env === MUSIC && <MusicSocial /> }
                     <button onClick={props.handleShow} id="contactButton">Contact</button>
-                    {
-                        props.env === DEV ? <Link to="/music">Music</Link>
-                    :   props.env === MUSIC ? <Link to="/dev">Dev</Link>
-                    :   null
-                    }
+                    { props.env === DEV && <Link to="/music">Music</Link> }
+                    { props.env === MUSIC && <Link to="/dev">Dev</Link> }
                 </nav>
             </header>
         )
