af41be604fc2540522d39b359b4f1a3aac53980b
Kameron Masullo <k_masullo@outlook.com>
2022-01-27 08:41:53 -0500

    Header.js refactor. Brand font exploration still in progress

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,21 +1,53 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import MusicSocial from '../music/MusicSocial';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpotify, faBandcamp, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from 'react-router-dom'
+import tidal from './img/tidal.svg'
 
-const DEV = "dev";
+const DEV   = "dev";
 const MUSIC = "music"
+
+const MUSIC_LISTEN_ITEMS = [
+    { label: "Spotify",  isFontAwesome: true,  icon: faSpotify,   href: "https://open.spotify.com/artist/5G946pHaGg1bWL8UjiZPdB?si=D0iBu9EsTSO5Vr8t-2MYfQ" },
+    { label: "Bandcamp", isFontAwesome: true,  icon: faBandcamp,  href: "https://kameronmasullo.bandcamp.com/" },
+    { label: "Tidal",    isFontAwesome: false, icon: <img src={tidal} alt='tidal-logo' style={{ color: 'white', width: '14px' }} />,  href: "https://tidal.com/browse/artist/21600507" }
+]
+
+const DEV_SOCIAL_ITEMS = [
+    { label: "LinkedIn", icon: faLinkedin, href: "https://linkedin.com/in/kameron-masullo" },
+    { label: "GitHub",   icon: faGithub,   href: "https://github.com/kmasullo" }
+]
 
 const Header = (props) => {
         return (
-            <header>
-                <Link to="/" className="headerLink"><h1 className="headerH1 hover">Kameron Masullo</h1></Link>
-                <nav>
-                    { props.env === MUSIC && <MusicSocial /> }
-                    <button onClick={props.handleShow} id="contactButton">Contact</button>
-                    { props.env === DEV && <Link to="/music">Music</Link> }
-                    { props.env === MUSIC && <Link to="/dev">Dev</Link> }
-                </nav>
-            </header>
+            <Navbar variant="dark" bg="rgb(19, 19, 19)" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand>
+                        <Link to="/" className='hoverOrange' style={{ fontFamily: "smythe", textDecoration: "none", letterSpacing: "4px", fontSize: "20px" }}>
+                            <h1>Kameron Masullo</h1>
+                        </Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbar" />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav>
+                            <NavDropdown title={props.env === MUSIC ? "Listen" : "Social"} menuVariant="dark" className='hoverOrange'>
+                                { ( props.env === MUSIC ? MUSIC_LISTEN_ITEMS : DEV_SOCIAL_ITEMS ).map(item => 
+                                    <NavDropdown.Item href={ item.href } target="_blank" rel="noopener noreferrer">
+                                        { item.isFontAwesome
+                                            ? <FontAwesomeIcon icon={ item.icon } size="sm" />
+                                            : item.icon }
+                                        {" " + item.label }
+                                    </NavDropdown.Item>
+                                )}
+                            </NavDropdown>
+                            <Nav.Link className='hoverOrange' onClick={props.handleShow}>Contact</Nav.Link>
+                            { props.env === DEV   && <Nav.Link href="/music" className='hoverOrange'>Music</Nav.Link> }
+                            { props.env === MUSIC && <Nav.Link href="/dev"   className='hoverOrange'>Dev</Nav.Link> }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         )
 }
 
